61213e1cb76a44fa909949039ddf1fa7cdd407ac
Hardik Yadav <hardikyadav@vertexcrm.dev>
2026-09-21 08:48:36 +0530

    fix(auth): import bcryptjs in auth controller to fix admin login server error

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ const AdminUser = require('../models/Admin');
 const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 const { OAuth2Client } = require('google-auth-library');
+const bcrypt = require('bcryptjs');
 
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
